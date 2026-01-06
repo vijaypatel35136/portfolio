d4a31520aa715cb8f3d80e8483624aca3ebacfcf
vijaypatel35136 <EMAIL_USED_IN_vijaypatel35136>
2026-01-06 14:27:50 +0530

    Updates

--- postcss.config.cjs
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-
-module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss')({ base: path.resolve(__dirname) }),
-    require('autoprefixer'),
-  ],
-}
--- src/data/messages.js
+++
@@ -1,4 +0,0 @@
-// Auto-saved from admin panel
-// Last updated: 1/6/2026, 12:10:47 PM
-
-export const initialMessages = [];
